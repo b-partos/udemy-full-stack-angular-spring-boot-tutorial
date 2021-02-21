eb166180cafd6e39714e863afbfa817d0457b749
partosb <bal.partos@gmail.com>
2021-02-21 13:09:46 +0100

    Sales person list created.

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
+
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Jozsi", "Kovacs", "jozsi.kovacs@gmail.com" ,  5000),
+    new SalesPerson("Geza", "Juhasz", "geza.juhasz@gmail.com" ,  6000)
+  ];
 
   constructor() { }
 
