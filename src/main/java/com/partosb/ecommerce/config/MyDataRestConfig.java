be0242fc5269caf807646193b8df70901405ba1e
partosb <bal.partos@gmail.com>
2021-02-23 09:01:01 +0100

    Cross origin support added and HTTP method permissions are set.

---
+++ src/main/java/com/partosb/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,26 @@
+package com.partosb.ecommerce.config;
+
+import com.partosb.ecommerce.entity.Product;
+import com.partosb.ecommerce.entity.ProductCategory;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        final HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+    }
+}
--- src/main/java/com/partosb/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/partosb/ecommerce/dao/ProductCategoryRepository.java
@@ -3,8 +3,9 @@ package com.partosb.ecommerce.dao;
 import com.partosb.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:6800/")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
